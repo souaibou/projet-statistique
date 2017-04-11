8b0041c8d263091c6b756d33becea9809d5d216d
souaibou <souaibou94@live.fr>
2017-04-11 18:17:56 +0000

    first commit

---
+++ jobmbayeProjectAngular/app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name jobmbayeProjectAngularApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the jobmbayeProjectAngularApp
+ */
+angular.module('jobmbayeProjectAngularApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
